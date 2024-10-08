d3b1b2f3f7109dc28e606b3e403c547ccc945d93
JhayDarko <jhojanderssj@gmail.com>
2024-10-08 14:30:12 -0500

    fix(X_ContactsBook)

--- X_ContactsBook.java
+++ X_ContactsBook.java
@@ -1,8 +1,9 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.TreeMap;
 
 public class X_ContactsBook {
-    TreeMap<String, String> contacts = new TreeMap<>();
+    TreeMap<String, Integer> contacts = new TreeMap<>();
     Scanner scan = new Scanner(System.in);
 
     public void addContact() {
@@ -16,18 +17,26 @@ public class X_ContactsBook {
                 return;
             }
         }
-        System.out.println("Ingresa el número de teléfono: ");
-        String number = scan.nextLine();
-        contacts.put(name, number);
-        System.out.println("Contacto agregado.");
+        while (true) { 
+            try {
+                System.out.println("Ingresa el número de teléfono: ");
+                Integer number = scan.nextInt();
+                contacts.put(name, number);
+                System.out.println("Contacto agregado.");
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Entrada no válida. Ingresa un número.");
+                scan.nextLine();
+            }
+        }
     }
 
     public void searchContact() {
         System.out.println("Ingresa el nombre del contacto a buscar: ");
         String name = scan.nextLine();
-        String number = contacts.get(name);
+        Integer number = contacts.get(name);
         if (number != null) {
-            System.out.printf("El número de %s es: %s", name, number);
+            System.out.printf("El número de %s es: %s.", name, number);
         } else {
             System.out.println("Contacto no encontrado.");
         }
@@ -36,6 +45,12 @@ public class X_ContactsBook {
     public void deleteContact() {
         System.out.println("Ingresa el nombre del contacto a eliminar: ");
         String name = scan.nextLine();
+        System.out.printf("¿Está seguro de que desea eliminar a '%s' de la lista de contactos? (Si/No)", name);
+        String response = scan.nextLine();
+        if (!response.equalsIgnoreCase("Si")) {
+            System.out.println("Operación cancelada.");
+            return;
+        }
         if (contacts.remove(name) != null) {
             System.out.println("Contacto eliminado.");
         } else {
@@ -55,31 +70,37 @@ public class X_ContactsBook {
     }
 
     public void exitApp() {
-        System.out.println("Saliendo de la agenda.");
+        System.out.println("Saliendo de la agenda...");
         scan.close();
     }
 
     public void run() {
         while (true) {
-            System.out.println("Elige una opción: ");
+            System.out.println("Elige una opción:");
             System.out.println("1. Añadir contacto");
             System.out.println("2. Buscar contacto");
             System.out.println("3. Ver todos los contactos");
             System.out.println("4. Eliminar contacto");
             System.out.println("5. Salir");
-            int opcion = scan.nextInt();
-            scan.nextLine();
 
-            switch (opcion) {
-                case 1 -> addContact();
-                case 2 -> searchContact();
-                case 3 -> showAllContacts();
-                case 4 -> deleteContact();
-                case 5 -> {
-                    exitApp();
-                    return;
+            try {
+                int opcion = scan.nextInt();
+                scan.nextLine();
+    
+                switch (opcion) {
+                    case 1 -> addContact();
+                    case 2 -> searchContact();
+                    case 3 -> showAllContacts();
+                    case 4 -> deleteContact();
+                    case 5 -> {
+                        exitApp();
+                        return;
+                    }
+                default -> System.out.println("Opción no válida. Ingresa un número relacionado a la opción.\n");
                 }
-                default -> System.out.println("Opción no válida. Inténtalo de nuevo.");
+            } catch (InputMismatchException e) {
+                System.out.println("Opción no válida. Ingresa un número relacionado a la opción.\n");
+                scan.nextLine();
             }
         }
     }
